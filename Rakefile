def22a081c8d593120e7bd53c48d46c2e574b836
Philipp Hansch <dev@phansch.net>
2017-03-11 16:59:47 +0100

    Move html proofer to rake task

--- Rakefile
+++ Rakefile
@@ -1,4 +1,5 @@
 require 'rake/testtask'
+require 'html-proofer'
 
 Rake::TestTask.new do |t|
   t.libs << 'lib'
@@ -7,3 +8,14 @@ Rake::TestTask.new do |t|
 end
 
 task default: :test
+
+task :html_proofer do
+  build_dir = File.join(File.dirname(__FILE__), '_site')
+  `jekyll build -d #{build_dir} -V` unless File.directory?('test/_site')
+  opts = {
+    url_ignore: [/localhost/],
+    empty_alt_ignore: true,
+    file_ignore: [/slides/]
+  }
+  HTMLProofer.check_directory(build_dir, opts).run
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,20 +3,9 @@ require 'minitest/autorun'
 require 'bundler/setup'
 require 'capybara/dsl'
 require 'rack/jekyll'
-require 'html-proofer'
 
-Capybara.app = Rack::Jekyll.new(force_build: true, destination: File.join(File.dirname(__FILE__), '_site'))
-
-def before_run
-  opts = {
-    url_ignore: [/localhost/],
-    empty_alt_ignore: true,
-    file_ignore: [/slides/]
-  }
-  HTMLProofer.check_directory(build_dir, opts).run
-end
-
-before_run
+build_dir = File.join(File.dirname(__FILE__), '_site')
+Capybara.app = Rack::Jekyll.new(force_build: true, destination: build_dir)
 
 class CapybaraTestCase < MiniTest::Test
   include Capybara::DSL
