9f122b74e9bff74bec774a9695e9e83c531bdb09
Philipp Hansch <dev@phansch.net>
2018-09-27 22:05:48 +0200

    Update blog page look

--- test/features/blog_index_test.rb
+++ test/features/blog_index_test.rb
@@ -1,13 +1,6 @@
 require 'test_helper'
 
 class BlogIndexTest < CapybaraTestCase
-  def test_read_more_link
-    visit '/writing'
-    assert page.has_content? 'Read more'
-    first('.comment_look').click_link('Read more')
-    assert page.has_content? 'min read'
-  end
-
   def test_dot_at_end_of_excerpt
     visit '/writing'
 
